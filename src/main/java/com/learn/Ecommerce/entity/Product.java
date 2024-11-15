58a8a69027a326ed970c6a32d67b1e260c5086f3
truptig-2512 <truptigorivale00@gmail.com>
2024-11-15 08:57:30 +0530

    2 commit

---
+++ src/main/java/com/learn/Ecommerce/entity/Product.java
@@ -0,0 +1,29 @@
+package com.learn.Ecommerce.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+//@Table(name="product_details") will help to change table name
+
+public class Product {
+
+	@Id
+	private int productId;
+	@Column(nullable = false)
+	private String productName;
+	@Column(name ="description",nullable = false)
+	private String productDescription;
+	@Column(nullable = false)
+	private int productPrice;
+	
+}

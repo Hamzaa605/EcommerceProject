c2e3622f4b9d0dfbc61ea9ae132b7127469e0cd2
truptig-2512 <truptigorivale00@gmail.com>
2024-11-28 09:00:55 +0530

    6 commit

--- src/main/java/com/learn/Ecommerce/repository/UserRepository.java
+++ src/main/java/com/learn/Ecommerce/repository/UserRepository.java
@@ -3,9 +3,20 @@ package com.learn.Ecommerce.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.learn.Ecommerce.entity.User;
+import java.util.List;
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User,String> {
 
+     Optional<User> findByEmailId(String emailId);
      
-	
+     
+     List<User> findByFirstNmae(String firstNmae);
+     
+     List<User> findByFirstNmaeStartingWith(String firstNmae);
+     
+     
+     
+     List<User> findByLastName(String lastName);
 }
